f1737d2c1cceb00ef74d11d720ac680a4be86115
leorenis <leorenis@gmail.com>
2022-07-06 21:13:50 -0300

    chore: Mono represents a streams of one or voidt

--- src/test/java/academy/leorenis/reactive/test/MonoTest.java
+++ src/test/java/academy/leorenis/reactive/test/MonoTest.java
@@ -2,12 +2,27 @@ package academy.leorenis.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
+    /**
+     * Mono represents a streams of one or void
+     * */
     @Test
-    public void test() {
+    public void monoSubscriber() {
+        var nome = "John Doe";
+        var mono = Mono.just(nome).log();
+
+        mono.subscribe();
+
+        StepVerifier.create(mono)
+                .expectNext(nome)
+                .verifyComplete();
+
+        log.info("Mono {}", mono);
         log.info("Setup");
     }
 }
