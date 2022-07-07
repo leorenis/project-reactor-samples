b3e0502b6fa299fbc990bd82e62bf69c3faeda30
leorenis <leorenis@gmail.com>
2022-07-06 21:53:30 -0300

    chore: Mono represents a streams of one or void

--- src/test/java/academy/leorenis/reactive/test/MonoTest.java
+++ src/test/java/academy/leorenis/reactive/test/MonoTest.java
@@ -2,8 +2,11 @@ package academy.leorenis.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.Locale;
 
 @Slf4j
 public class MonoTest {
@@ -55,5 +58,74 @@ public class MonoTest {
         StepVerifier.create(mono)
                 .expectError(RuntimeException.class)
                 .verify();
+    }
+
+
+    @Test
+    public void monoSubscriberConsumerComplete() {
+        var nome = "John Doe";
+        var mono = Mono.just(nome)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(
+                s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("Finished")
+        );
+
+        log.info("---------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(nome.toUpperCase())
+                .verifyComplete();
+    }
+
+
+    @Test
+    public void monoSubscriberConsumerSubscription() {
+        var nome = "John Doe";
+        var mono = Mono.just(nome)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(
+                s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("Finished"),
+                Subscription::cancel
+        );
+
+        log.info("---------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(nome.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoDoOnMethods() {
+        var nome = "John Doe";
+        var mono = Mono.just(nome)
+                .log()
+                .map(String::toUpperCase)
+                .doOnSubscribe(s -> log.info("Subscribed {}", s))
+                .doOnRequest(longNumber -> log.info("Request received, starting doing smb"))
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s))
+                .flatMap(s -> Mono.empty())
+                .doOnNext(s -> log.info("Value is here. Executing doOnNext {}", s))
+                .doOnSuccess(s -> log.info("doOnSuccess Executed {}", s));
+
+        mono.subscribe(
+                s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("Finished")
+        );
+
+        log.info("---------------------");
+
+        /*StepVerifier.create(mono)
+                .expectNext(nome.toUpperCase())
+                .verifyComplete();*/
     }
 }
